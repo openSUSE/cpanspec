6e3d6cf854dabeb2fa220db2cd56bc370d5574a6
Stephan Kulow <coolo@suse.de>
2015-11-10 11:55:42 +0100

    try to be clever about items

--- PodViewSpec.pm
+++ PodViewSpec.pm
@@ -1,5 +1,7 @@
 package PodViewSpec;
-use base "Pod::POM::View::Text";
+use parent qw( Pod::POM::View::Text );
+use Pod::POM::View;
+use Text::Wrap;
 
 # overwrite the link default - we don't want
 # to reference to "the manpage"
@@ -8,5 +10,25 @@ sub view_seq_link {
     return $link;
 }
 
+sub view_item {
+    my ($self, $item) = @_;
+    my $indent = ref $self ? \$self->{ INDENT } : \$INDENT;
+    my $pad = ' ' x $$indent;
+    local $Text::Wrap::unexpand = 0;
+    my $title = $item->title->present($self);
+    if ($title !~ m/^\s*$/ && $title ne '*') {
+	$title = wrap($pad . '* ', $pad . '  ', $title);
+
+	$$indent += 2;
+	my $content = $item->content->present($self);
+	$$indent -= 2;
+
+	return "$title\n\n$content";
+    } else {
+	my $content = $item->content->present($self);
+	chomp $content;
+	return "  * $content\n";
+    }
+}
 
 1;
